97167f7a40e9ce411cb329a87ea96a298ab82e2f
kazurayam <kazuaki.urayama@gmail.com>
2025-09-09 20:50:46 +0900

    save updates

---
+++ README.md
@@ -0,0 +1,26 @@
+# プロキシサーバの作り方
+
+https://monotein.com/books/proxy-server-book
+
+
+## プロキシサーバを起動する
+
+```
+$ cd $projectdir
+$ node index.js
+```
+
+または
+
+```
+$ cd $projectidr
+$ nodemon
+```
+
+## プロキシサーバを停止する
+
+```
+$ cd $projectdir
+```
+
+and then CTRL+C
--- index.js
+++ index.js
@@ -2,11 +2,25 @@
 
 const express = require("express")
 const app = express()
+const { createProxyMiddleware } = require("http-proxy-middleware")
 
 app.get("/", (req, res) => {
     res.send("This is my proxy server")
 })
 
-app.listen(5050, () => {
+app.use("/corona-tracker-world-data", (req, res, next) => {
+    createProxyMiddleware({
+        target: "https://monotein-books.vercel.app/api/corona-tracker/summary",
+        changeOrigin: true,
+        pathRewrite: {
+            [`^/corona-tracker-world-data`]: "",
+        },
+    })(req, res, next)
+})
+
+const port = process.env.PORT || 5050
+
+app.listen(port, () => {
     console.log("Listening on localhost port 5050")
 })
+
